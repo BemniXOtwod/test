2cd3907ebf79757698c8697dd4f970bd43b37c5c
Btsegaye258@gmail.com <btsegaye258@gmail.com>
2024-05-24 01:17:51 +0300

    Added a route file

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -39,4 +39,25 @@ export const register = async (req, res) => {
     catch (err){
         res.status(500).json({ error: err.message })
     }
+};
+
+/* Logging in*/
+
+export const login = async (req, res) => {
+  try{ 
+    const { email, password} = req.body;
+    const user = await User.findOne({email: email});
+    if (!user) return res.status(400).json({ msg: " User doesn't exist. "});
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch) return res.status(400).json({ msg: " Invalid Credentioals"});
+  
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+  delete user.password;
+  res.status(200).json({ token, user });
+}
+  catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+
 }
