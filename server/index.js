e8713c9bcd7b8440a0b3862366b36e54f7f9e3bd
Btsegaye258@gmail.com <btsegaye258@gmail.com>
2024-05-27 14:52:53 +0300

    refresh

--- server/index.js
+++ server/index.js
@@ -14,6 +14,8 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "../models/User.js";
+import { users, posts } from "./data/index.js";
 
 /* Configurations */
 
@@ -62,5 +64,10 @@ mongoose.connect(process.env.MONGO_URL, {
 })
 .then(() => {
  app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+
+ /* ADD DATA ONE TIME */
+ //User.insertMany(users);
+ //post.insertMany(posts);
 })
 .catch((error) => console.log(`${error} did not connect`));
