c957b1c388c52ae8c4fc522fd844b3af86b22e4f
Btsegaye258@gmail.com <btsegaye258@gmail.com>
2024-05-27 12:42:50 +0300

    Add friend and liking posts included

--- server/index.js
+++ server/index.js
@@ -9,7 +9,11 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import usersRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 /* Configurations */
 
@@ -42,10 +46,12 @@ const upload = multer({ storage });
 /*Routes with Files */
 
 app.post("/auth/register", upload.single("picture"), register);
-
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* routes*/
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /* Mongoose setup */
 
